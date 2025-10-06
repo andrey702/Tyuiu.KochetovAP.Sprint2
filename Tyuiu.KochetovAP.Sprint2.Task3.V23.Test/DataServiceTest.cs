d31c58e654f04b276c0fe7c4930877fa7f0c2e3e
andreykochetov <oliverr223344@gmail.com>
2025-10-06 16:05:02 +0600

    Task3

--- Tyuiu.KochetovAP.Sprint2.Task3.V23.Test/DataServiceTest.cs
+++ Tyuiu.KochetovAP.Sprint2.Task3.V23.Test/DataServiceTest.cs
@@ -9,8 +9,8 @@ namespace Tyuiu.KochetovAP.Sprint2.Task3.V23.Test
         public void TestMethod1()
         {
             DataService service = new DataService();
-            Assert.AreEqual(2 * Math.Pow(3, 2), service.Calculate(2), 0.001);
-            Assert.AreEqual(-16, service.Calculate(0), 0.001);
+            Assert.AreEqual(Math.Round(2 * Math.Pow(3, 2), 3), Math.Round(service.Calculate(2), 3));
+            Assert.AreEqual(Math.Round((double)-16, 3), Math.Round(service.Calculate(0), 3));
 
         }
     }
